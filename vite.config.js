0975572b2f493702e34ef7d038b9f25b53d9a0a5
jason <jaml08@hotmail.com>
2025-10-12 06:14:16 -0500

    refact: habilite cache

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: [
+      "firebase/app",
+      "firebase/auth",
+      "firebase/firestore",
+      "framer-motion",
+    ],
+  },
+  cacheDir: "node_modules/.vite_cache",
 });
